08216580134c4f3a74f3a0220027fbefbd5415ca
Pete A <peter.ayresmusic@gmail.com>
2019-03-14 00:32:34 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,8 +5,16 @@ birthday_kids = {
 }
 
 
-  def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
+def happy_birthday(birthday_kids)
+birthday_kids.each do |kids_name, age|
+  puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+end
+end
+
+def age_appropriate_birthday(birthday_kids)
+birthday_kids.each do |kids_name, age|
+  if age < 12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
+end 
